4c7a9501f1f6d0d51899e90a4124d999292a2831
sswetank <sswetank@cognitivescale.com>
2020-02-29 00:26:18 +0530

    Varied Exploration and Exploitation ratios and performance on estimated value for an action

--- RL/bandits/exercise2.5.py
+++ RL/bandits/exercise2.5.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 # Adding non-stationary rewards
 def calc_reward():
 	# np.random.RandomState(1234)
-	return np.random.normal(0, 0.01)
+	return np.random.normal(0, 1)
 
 
-def simple_avg_non_stationary_bandit():
+def simple_avg_non_stationary_bandit(counter_max, prob_ctr_thres):
 	# k actions Lets assume k=10
 	k = 10
 	# Initialize the value for all the k actions as 0
@@ -24,14 +24,14 @@ def simple_avg_non_stationary_bandit():
 	# Loop forever (By forever means very long term say 20k times)
 	counter = 0
 	prob_ctr = 0
-	while counter < 5000:
+	while counter < counter_max:
 		rewards = {}
 		counter += 1
 		prob_ctr += 1
 		for i in range(1, 11):
 			rewards[i] = calc_reward()
 
-		if prob_ctr < 100:
+		if prob_ctr < prob_ctr_thres:
 			# Exploitation
 			# Get the best action
 			action, max_rewards = sorted(rewards.items(), key=lambda kv: (kv[1], kv[0]))[-1]
@@ -48,23 +48,38 @@ def simple_avg_non_stationary_bandit():
 	# Update the average rewards for each action
 
 
-def plot(reward_action):
-
-	x=range(len(reward_action))
-	plt.plot(x, reward_action)
+def plot(reward_actions):
+	for reward_action in reward_actions:
+		x=range(len(reward_action))
+		plt.plot(x, reward_action)
 	plt.show()
 
 
-if __name__ == '__main__':
-	reward_action = simple_avg_non_stationary_bandit()
+def get_max_reward(reward_action):
 	best_action = 0
-	max_reward=0
+	max_reward = 0
 	for k in reward_action.keys():
 		if sum(reward_action[k]) > max_reward:
 			best_action = k
 			max_reward = sum(reward_action[k])
+	return best_action, max_reward
+
+
+if __name__ == '__main__':
+	reward_action = simple_avg_non_stationary_bandit(10000, 2)
+	reward_action1 = simple_avg_non_stationary_bandit(10000, 10)
+	reward_action2 = simple_avg_non_stationary_bandit(10000, 1000)
+	# best_action = 0
+	# max_reward=0
+	# for k in reward_action.keys():
+	# 	if sum(reward_action[k]) > max_reward:
+	# 		best_action = k
+	# 		max_reward = sum(reward_action[k])
 	# plot(reward_action[best_action])
-	print(max_reward)
-	print(best_action)
+	best_action, max_reward = get_max_reward(reward_action)
+	best_action1, max_reward1 = get_max_reward(reward_action1)
+	best_action2, max_reward2 = get_max_reward(reward_action2)
+	print(max_reward,max_reward1, max_reward2)
+	print(best_action, best_action1, best_action2)
 	print(len(reward_action[best_action]))
-	plot(reward_action[best_action])
+	plot([reward_action[best_action],reward_action1[best_action1], reward_action2[best_action2]])
